24424cbde217ebee8cb5bdebce02d3a25c7b3d1a
Priyam Anand <priyam27.anand@gmail.com>
2021-11-21 19:32:47 +0530

    added vanta js to landing page

--- client/src/Components/Info/Info.jsx
+++ client/src/Components/Info/Info.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import "./Info.css";
+import CLOUDS from 'vanta/dist/vanta.clouds.min'
 
 const Info = () => {
+
+    const [vantaEffect, setVantaEffect] = useState(0)
+    const myRef = useRef(null)
+    useEffect(() => {
+        if (!vantaEffect) {
+            setVantaEffect(
+                CLOUDS({
+                    el: myRef.current,
+                    mouseControls: true,
+                    touchControls: true,
+                    gyroControls: false,
+                    minHeight: 200.00,
+                    minWidth: 200.00
+                })
+            )
+        }
+        return () => {
+            if (vantaEffect) vantaEffect.destroy()
+        }
+    }, [vantaEffect])
+
     return (
         <>
-            <div id="hero" className="d-flex align-items-center">
+            <div id="hero" ref={myRef} className="d-flex align-items-center">
                 <div className="container">
                     <h1>Welcome to D-Med</h1>
                     <h2>The Future of medical records is BLOCKCHAIN</h2>
