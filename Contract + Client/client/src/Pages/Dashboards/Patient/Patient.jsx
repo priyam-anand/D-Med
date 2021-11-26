c52c592904dc2710443929178f61cf7a5dfe70e3
Priyam Anand <priyam27.anand@gmail.com>
2021-11-26 20:08:55 +0530

    patient dashboard complete

--- Contract + Client/client/src/Pages/Dashboards/Patient/Patient.jsx
+++ Contract + Client/client/src/Pages/Dashboards/Patient/Patient.jsx
@@ -13,6 +13,14 @@ const Patient = () => {
     const [accounts, setAccounts] = useState(undefined);
     const [contract, setContract] = useState(undefined);
     const history = useHistory();
+    const [yourId, setYourId] = useState("");
+    const [patient, setPatient] = useState(null);
+    const [yourIdRec, setYourIdRec] = useState("");
+    const [records, setRecords] = useState([]);
+    const [addAuthId, setAddAuthId] = useState("");
+    const [addAuthAddress, setAddAuthAddress] = useState("");
+    const [removeAuthId, setRemoveAuthId] = useState("");
+    const [removeAuthAddress, setRemoveAuthAddress] = useState("");
 
     const isReady = () => {
         return (
@@ -33,7 +41,6 @@ const Patient = () => {
                     DMed.abi,
                     deployedNetwork && deployedNetwork.address,
                 );
-
                 setWeb3(web3);
                 setAccounts(accounts);
                 setContract(contract);
@@ -50,6 +57,88 @@ const Patient = () => {
         }
     }, [history]);
 
+    const getPatient = async (e) => {
+        e.preventDefault();
+        try {
+            const pat = await contract.methods.getPatientDetails(yourId).call({ from: accounts[0] });
+            setPatient(pat);
+        } catch (err) {
+            window.alert("Could not get details of patient. Please make sure you have the correct rights and you have the correct Id")
+        }
+        setYourId("");
+    }
+
+    const getRecords = async (e) => {
+        e.preventDefault();
+        setRecords([]);
+        try {
+            const recs = await contract.methods.getPatientRecords(yourIdRec).call({ from: accounts[0] });
+            const recsArray = [];
+            for (let i = 0; i < recs[0].length; i++) {
+                recsArray.push({
+                    hospitalId: recs[0][i],
+                    condition: recs[1][i],
+                    description: recs[2][i],
+                    allergy: recs[3][i],
+                    docs: recs[4][i]
+                })
+            }
+            setRecords(recsArray);
+        } catch (err) {
+            console.log(err)
+            window.alert("Could not get records of patient. Please make sure you have the correct rights and you have the correct Id")
+        }
+        setYourIdRec("");
+    }
+
+    const handleAddAuthAddress = async (e) => {
+        e.preventDefault();
+        try {
+            await contract.methods.addAuthByAddress(addAuthAddress).send({ from: accounts[0] });
+            window.alert("Authorized successfully");
+        } catch (error) {
+            window.alert("Could not authorized. Make sure you have the right privileges");
+            console.error(error);
+        }
+        setAddAuthAddress("");
+    }
+
+    const handleAddAuthId = async (e) => {
+        e.preventDefault();
+        try {
+            await contract.methods.addAuthById(addAuthId).send({ from: accounts[0] });
+            window.alert("Authorized successfully");
+        } catch (error) {
+            window.alert("Could not authorized. Make sure you have the right privileges");
+            console.error(error);
+        }
+        setAddAuthId("");
+    }
+
+    const handleRemoveAuthAddress = async (e) => {
+        e.preventDefault();
+        try {
+            await contract.methods.revokeAuthByAddress(removeAuthAddress).send({ from: accounts[0] });
+            window.alert("Unauthorized successfully");
+        } catch (error) {
+            window.alert("Could not authorized. Make sure you have the right privileges.");
+            console.error(error);
+        }
+        setRemoveAuthAddress("");
+    }
+
+    const handleRemoveAuthId = async (e) => {
+        e.preventDefault();
+        try {
+            await contract.methods.revokeAuthById(removeAuthId).send({ from: accounts[0] });
+            window.alert("Unauthorized successfully");
+        } catch (error) {
+            window.alert("Could not authorized. Make sure you have the right privileges.");
+            console.error(error);
+        }
+        setRemoveAuthId("");
+    }
+
     if (!isReady()) {
         return <Loading />;
     }
@@ -65,12 +154,24 @@ const Patient = () => {
                         </p>
                     </div>
 
-                    <div className="patient-detail">
-                        <h4 style={{ "textAlign": "center" }}>
-                            Your details
+                    <div className="get-details">
+                        <h4>
+                            Get your Details
                         </h4>
                         <div>
-                            <Patient1 />
+                            <form onSubmit={getPatient}>
+                                <div className="row">
+                                    <div className="col-md-4 form-group py-1">
+                                        <input type="text" name="address" className="form-control" id="patient-address" placeholder="Your Id" required value={yourId} onChange={e => setYourId(e.target.value)} />
+                                    </div>
+                                    <div className="col-md-4 form-group mt-3 mt-md-0 py-1">
+                                        <button type="submit">Click Here</button>
+                                    </div>
+                                </div>
+                            </form>
+                            {!patient
+                                ? <></>
+                                : <Patient1 patient={patient} />}
                         </div>
                     </div>
 
@@ -79,19 +180,23 @@ const Patient = () => {
                             Get your Record
                         </h4>
                         <div>
-                            <form>
+                            <form onSubmit={getRecords}>
                                 <div className="row">
                                     <div className="col-md-4 form-group py-1">
-                                        <input type="text" name="address" className="form-control" id="patient-address" placeholder="Your Address" />
+                                        <input type="text" name="address" className="form-control" id="patient-address" placeholder="Your Id" value={yourIdRec} onChange={e => setYourIdRec(e.target.value)} required />
                                     </div>
                                     <div className="col-md-4 form-group mt-3 mt-md-0 py-1">
                                         <button type="submit">Click Here</button>
                                     </div>
                                 </div>
                             </form>
-                            <Record />
-                            <Record />
-                            <Record />
+                            {records.length > 0
+                                ?
+                                records.map((record, index) => {
+                                    return <div key={index}><Record record={record} /></div>
+                                })
+                                : <></>
+                            }
                         </div>
                     </div>
                     <div className="get-details">
@@ -99,13 +204,23 @@ const Patient = () => {
                             Give Authorization
                         </h4>
                         <div>
-                            <form>
+                            <form onSubmit={handleAddAuthAddress}>
                                 <div className="row">
                                     <div className="col-md-4 form-group py-1">
-                                        <input type="text" name="address" className="form-control" id="org-address" placeholder="Organization's Address" />
+                                        <input type="text" name="address" className="form-control" id="org-address" placeholder="Organization's Address" required value={addAuthAddress} onChange={e => setAddAuthAddress(e.target.value)} />
                                     </div>
+                                    <div className="col-md-4 form-group mt-3 mt-md-0 py-1">
+                                        <button type="submit">Click Here</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <span className="or">
+                                Or
+                            </span>
+                            <form onSubmit={handleAddAuthId}>
+                                <div className="row">
                                     <div className="col-md-4 form-group py-1">
-                                        <input type="number" name="id" className="form-control" id="org-id" placeholder="Organization's Id" />
+                                        <input type="text" name="address" className="form-control" id="org-address" placeholder="Organization's Id" required value={addAuthId} onChange={e => setAddAuthId(e.target.value)} />
                                     </div>
                                     <div className="col-md-4 form-group mt-3 mt-md-0 py-1">
                                         <button type="submit">Click Here</button>
@@ -119,13 +234,23 @@ const Patient = () => {
                             Remove Authorization
                         </h4>
                         <div>
-                            <form>
+                            <form onSubmit={handleRemoveAuthAddress}>
                                 <div className="row">
                                     <div className="col-md-4 form-group py-1">
-                                        <input type="text" name="address" className="form-control" id="org-address" placeholder="Organization's Address" />
+                                        <input type="text" name="address" className="form-control" id="org-address" placeholder="Organization's Address" value={removeAuthAddress} onChange={e=>setRemoveAuthAddress(e.target.value)} required/>
                                     </div>
+                                    <div className="col-md-4 form-group mt-3 mt-md-0 py-1">
+                                        <button type="submit">Click Here</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <span className="or">
+                                Or
+                            </span>
+                            <form onSubmit={handleRemoveAuthId}>
+                                <div className="row">
                                     <div className="col-md-4 form-group py-1">
-                                        <input type="number" name="id" className="form-control" id="org-id" placeholder="Organization's Id" />
+                                        <input type="text" name="address" className="form-control" id="org-address" placeholder="Organization's Id" value={removeAuthId} onChange={e=>setRemoveAuthId(e.target.value)} required/>
                                     </div>
                                     <div className="col-md-4 form-group mt-3 mt-md-0 py-1">
                                         <button type="submit">Click Here</button>
